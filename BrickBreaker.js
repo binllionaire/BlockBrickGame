52c2c7f56a6e442e9a9be8d3c7597470903afaaf
binllionaire <46593078+binllionaire@users.noreply.github.com>
2022-05-23 21:46:52 +0900

    subin

--- BrickBreaker.js
+++ BrickBreaker.js
@@ -1,9 +1,11 @@
 $(document).ready(function(){
   $("#startButton").on("click", intro);
   $("#intro p:nth-child(2)").on("click",gameMenu);
-  $("#game1Button").on("click", game1);
-  $("#game2Button").on("click", game2);
-  $("#game3Button").on("click", game3);
+  $("#menu1").on("click", game1);
+  $("#menu2").on("click", game2);
+  $("#menu3").on("click", game3);
+
+
 });
 
 function intro(){
