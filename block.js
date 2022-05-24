05977850d2303cf733e8d36b5b57c4038e960341
Chungjaewoo <qufekf989@naver.com>
2022-05-25 00:35:01 +0900

    m

---
+++ block.js
@@ -0,0 +1,30 @@
+
+
+$(document).ready(function(){
+    $("#startButton").on("click", intro);
+    $("#intro p:nth-child(2)").on("click",gameMenu);
+    $("#game1Button").on("click", game1);
+    $("#game2Button").on("click", game2);
+    $("#game3Button").on("click", game3);
+  });
+  
+  function intro(){
+    $("#main-menu").css("display","none"); 
+    $("#intro").fadeIn( 2000 );
+  }
+  function gameMenu(){
+    $("#intro").css("display","none"); 
+    $("#game-menu").fadeIn( 2000 );
+  }
+  function game1(){
+    $("#game-menu").css("display","none"); 
+    $("#game1").css("display","block"); 
+  }
+  function game2(){
+    $("#game-menu").css("display","none"); 
+    $("#game2").css("display","block"); 
+  }
+  function game3(){
+    $("#game-menu").css("display","none"); 
+    $("#game3").css("display","block"); 
+  }
